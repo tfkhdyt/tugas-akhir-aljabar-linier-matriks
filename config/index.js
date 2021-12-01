495a1976ab053c38501c16d51b8db97bb495df32
tfkhdyt <ta142fa@gmail.com>
2021-12-01 19:11:58 +0700

    nambah component navbar

---
+++ config/index.js
@@ -0,0 +1,4 @@
+export default {
+  judul: 'Tugas Akhir Aljabar Linier & Matriks',
+  logo: 'https://fti.unibba.ac.id/wp-content/uploads/2016/07/cropped-Logo-FTI-UNIBBA.png'
+}
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+      domains: ['fti.unibba.ac.id']
+  }
+}
