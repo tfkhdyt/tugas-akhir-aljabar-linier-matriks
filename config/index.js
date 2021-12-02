511348bef06dd0d886e60d5f3c16a4689bf4699f
tfkhdyt <ta142fa@gmail.com>
2021-12-02 12:40:57 +0700

    gunakan staticprops

--- config/index.js
+++ config/index.js
@@ -6,40 +6,6 @@ export default {
   link: 'https://tugas-akhir-aljabar-linier-matriks.vercel.app',
   deskripsi:
   'Tugas Akhir Aljabar Linier & Matriks menggunakan framework Next.js dan Tailwind CSS',
-  thumbnail: '/images/thumbnail.png',
-  member: [{
-    nama: "Taufik Hidayat",
-    nim: "301200032",
-    prodi: "Teknik Informatika",
-    foto: "/images/member/taufik.jpg",
-    ig: "_tfkhdyt_"
-  },
-    {
-      nama: "Dadan Hardiansah",
-      nim: "301200001",
-      prodi: "Teknik Informatika",
-      foto: "/images/member/dadan.jpg",
-      ig: "dadanha_"
-    },
-    {
-      nama: "Tuhfah Taj Nabilah",
-      nim: "302200005",
-      prodi: "Sistem Informasi",
-      foto: "/images/member/tuhfah.jpg",
-      ig: "tuhfahtn00"
-    },
-    {
-      nama: "Wira Restu Wibawa",
-      nim: "301200008",
-      prodi: "Teknik Informatika",
-      foto: "/images/member/wira.jpg",
-      ig: "wirarw33"
-    },
-    {
-      nama: "Nur Muhamad Faisal",
-      nim: "301200004",
-      prodi: "Teknik Informatika",
-      foto: "/images/member/faisal.jpg",
-      ig: "nmf23_"
-    }]
+  thumbnail: '/images/thumbnail.png'
+  /**/
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -2,9 +2,8 @@ import Head from 'next/head'
 import Data from '../../config'
 import { MemberCard } from '../../components/MemberCard'
 
-/*export async function getStaticProps (context) {
-  // const res = await fetch(`${Data.link}/member.json`)
-  const res = await fetch(`http://localhost:3000/member.json`)
+export async function getStaticProps (context) {
+  const res = await fetch(`${Data.link}/member.json`)
   const data = await res.json()
   // console.log(data.member)
   return {
@@ -12,7 +11,7 @@ import { MemberCard } from '../../components/MemberCard'
       member: data.member
     }
   }
-}*/
+}
 
 export default function Home (props) {
   return (
@@ -26,7 +25,7 @@ export default function Home (props) {
       <main className='container mx-auto p-6 lg:pl-12 relative'>
         <span className='text-2xl font-semibold'>{Data.menu[1]}</span>
         <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
-          {Data.member.map((e) => {
+          {props.member.map((e) => {
             return (
               <div key={e.id}>
                 <MemberCard
