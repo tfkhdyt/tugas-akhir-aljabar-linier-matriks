236319778b0e02165a41fa112dfa8c485078e808
tfkhdyt <ta142fa@gmail.com>
2021-12-04 15:55:08 +0700

    bikin form untuk matriks

--- components/Content.jsx
+++ components/Content.jsx
@@ -3,18 +3,124 @@
 // import { useState } from 'react'
 import { Provider, Node } from '@nteract/mathjax'
 
+const Table = () => {
+  return (
+    <form>
+      <div className='hover:shadow w-full transition duration-500 ease-in-out'>
+        <table className='w-full table-fixed text-center'>
+          <tbody className='bg-white divide-y divide-gray-200'>
+            {/* baris satu */}
+            <tr className='divide-x divide-gray-200'>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[1,1]'
+                  name='_11'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[1,2]'
+                  name='_12'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[1,3]'
+                  name='_13'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+            </tr>
+            {/* baris dua */}
+            <tr className='divide-x divide-gray-200'>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[2,1]'
+                  name='_21'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[2,2]'
+                  name='_22'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[2,3]'
+                  name='_23'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+            </tr>
+            {/* baris tiga */}
+            <tr className='divide-x divide-gray-200'>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[3,1]'
+                  name='_31'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[3,2]'
+                  name='_32'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+              <td className='p-2'>
+                <input
+                  type='number'
+                  placeholder='[3,3]'
+                  name='_33'
+                  className='w-full text-center font-semibold outline-none p-2 tracking-widest'
+                  required
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <button
+        type='submit'
+        className='bg-blue-400 px-4 py-2 text-white mt-5 rounded-md hover:bg-blue-500 hover:shadow-md font-semibold'
+      >
+        Hitung
+      </button>
+    </form>
+  )
+}
+
 const Form = () => {
   return (
-    <div className='w-full bg-blue-50 hover:bg-blue-100 rounded-lg shadow-sm hover:shadow-lg p-6 flex flex-col justify-center items-center transition duration-500 ease-in-out text-gray-600 text-justify'>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <div className='w-auto bg-blue-50 hover:bg-blue-100 rounded-lg shadow-sm hover:shadow-lg p-6 flex flex-col transition duration-500 ease-in-out'>
+      <div>
+        <p className='font-semibold text-gray-600 text-left mb-3'>
+          Masukkan matriks 3x3:
+        </p>
+      </div>
+      <Table />
     </div>
   )
 }
@@ -23,13 +129,15 @@ const Hasil = () => {
   const tex = String.raw`\begin{bmatrix}6 & 9 \\4 & 2 \end{bmatrix} + \begin{bmatrix}1 & 2 \\2 & 3 \end{bmatrix} = \ ?`
   return (
     <div>
-      <div className='w-full bg-blue-50 hover:bg-blue-100 rounded-lg shadow-sm hover:shadow-lg p-6 transition duration-500 ease-in-out'>
+      <div className='w-auto bg-blue-50 hover:bg-blue-100 rounded-lg shadow-sm hover:shadow-lg p-6 transition duration-500 ease-in-out'>
         <p className='text-xl font-semibold mb-4 text-gray-800'>Hasil</p>
         <p className='text-center text-gray-600'>
-          <Provider options={{
-            showMathMenu: false,
-            messageStyle: "none"
-          }}>
+          <Provider
+            options={{
+              showMathMenu: false,
+              messageStyle: 'none'
+            }}
+          >
             <Node>{tex}</Node>
           </Provider>
         </p>
@@ -40,7 +148,7 @@ const Hasil = () => {
 
 export const Content = (props) => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 justify-items-stretch w-full'>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-8 justify-items-stretch w-full'>
       <Form />
       <Hasil />
     </div>
--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export default {
   judul: 'Tugas Akhir Aljabar Linier & Matriks',
   logo: 'https://fti.unibba.ac.id/wp-content/uploads/2016/07/cropped-Logo-FTI-UNIBBA.png',
-  menu: ['Home', 'About Us'],
+  menu: ['Invers Matriks 3x3', 'About Us'],
   link: 'https://tugas-akhir-aljabar-linier-matriks.vercel.app',
   deskripsi:
     'Tugas Akhir Aljabar Linier & Matriks menggunakan framework Next.js dan Tailwind CSS',
