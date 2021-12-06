6cf4e003ea8fc1ac1139faa8ad4f9a0b38e7de67
Taufik Hidayat <tfkhdyt@pm.me>
2021-12-06 12:00:37 +0700

    nambah transisi di component hasil

--- components/Hasil.jsx
+++ components/Hasil.jsx
@@ -6,7 +6,7 @@ import MatrixContext from './MatrixContext'
 export default function Hasil () {
   const { matrix } = useContext(MatrixContext)
   return (
-    <div className='w-auto bg-blue-50 hover:bg-blue-100 rounded-lg shadow-sm hover:shadow-lg p-6 flex flex-col'>
+    <div className='w-auto bg-blue-50 hover:bg-blue-100 rounded-lg shadow-sm hover:shadow-lg p-6 flex flex-col transition duration-500'>
       <p className='text-xl font-semibold text-gray-800'>Hasil</p>
       <div>
         <p className='text-left text-gray-600'>
