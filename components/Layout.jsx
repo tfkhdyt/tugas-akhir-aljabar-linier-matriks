9bfa6eab9b09709b9e51ea5802d4366ba185f066
tfkhdyt <ta142fa@gmail.com>
2021-12-09 22:37:08 +0700

    fix metadata dan ubah efek transisi toast ke slide

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,28 +1,52 @@
 import { useRouter } from 'next/router'
-import { ToastContainer } from 'react-toastify'
+import Head from 'next/head'
+import { ToastContainer, Slide } from 'react-toastify'
 
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Data from '../config'
 import 'react-toastify/dist/ReactToastify.min.css'
 
 export default function Layout ({ children }) {
   const router = useRouter()
   return (
-    <div>
-      <ToastContainer
-        position='top-right'
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={router.pathname !== '/404'}
-        rtl={false}
-        pauseOnFocusLoss={router.pathname !== '/404'}
-        draggable={router.pathname !== '/404'}
-        pauseOnHover={router.pathname !== '/404'}
-      />
-      {router.pathname !== '/404' ? <Navbar /> : null}
-      {children}
-      {router.pathname !== '/404' ? <Footer /> : null}
-    </div>
+    <>
+      <Head>
+        <meta name='description' content={Data.deskripsi} />
+        <meta
+          name='google-site-verification'
+          content='JCu7ig2hkiijnjnq8doWrgNg9HPCpWwo2WrTQWko8Cs'
+        />
+        <meta name='keywords' content={Data.keywords.join(', ')} />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:description' content={Data.deskripsi} />
+        <meta name='twitter:image' content={Data.thumbnail} />
+        <meta name='twitter:title' content={Data.judul} />
+        <meta property='og:description' content={Data.deskripsi} />
+        <meta property='og:image' content={Data.thumbnail} />
+        <meta property='og:title' content={Data.judul} />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={Data.link} />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='image_src' href={Data.thumbnail} />
+      </Head>
+      <div>
+        <ToastContainer
+          position='top-right'
+          transition={Slide}
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={router.pathname !== '/404'}
+          rtl={false}
+          pauseOnFocusLoss={router.pathname !== '/404'}
+          draggable={router.pathname !== '/404'}
+          pauseOnHover={router.pathname !== '/404'}
+        />
+        {router.pathname !== '/404' ? <Navbar /> : null}
+        {children}
+        {router.pathname !== '/404' ? <Footer /> : null}
+      </div>
+    </>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import Data from '../config'
-
 export default function Document () {
   return (
     <Html>
       <Head>
-        <meta name='description' content={Data.deskripsi} />
-        <meta
-          name='google-site-verification'
-          content='JCu7ig2hkiijnjnq8doWrgNg9HPCpWwo2WrTQWko8Cs'
-        />
-        <meta name='keywords' content={Data.keywords.join(', ')} />
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:description' content={Data.deskripsi} />
-        <meta name='twitter:image' content={Data.thumbnail} />
-        <meta name='twitter:title' content={Data.judul} />
-        <meta property='og:description' content={Data.deskripsi} />
-        <meta property='og:image' content={Data.thumbnail} />
-        <meta property='og:title' content={Data.judul} />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content={Data.link} />
-        <link rel='icon' href='/favicon.ico' />
-        <link rel='image_src' href={Data.thumbnail} />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
