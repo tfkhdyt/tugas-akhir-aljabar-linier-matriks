a23c642b523d76056ed7e31a7299e9436d21e9ec
tfkhdyt <ta142fa@gmail.com>
2021-12-09 16:10:39 +0700

    buat tinggi membercard konsisten

--- pages/about/index.js
+++ pages/about/index.js
@@ -22,7 +22,7 @@ export default function About ({ member }) {
       </Head>
       <main className='container mx-auto p-6 lg:px-24 relative'>
         <span className='text-2xl font-semibold'>{Data.menu[3]}</span>
-        <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+        <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 flex items-stretch'>
           {member.map((e, i) => {
             return (
               <div key={i}>
