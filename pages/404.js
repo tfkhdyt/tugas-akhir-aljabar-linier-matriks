88199b377282942f0c571e6ea15168fce073c228
tfkhdyt <ta142fa@gmail.com>
2021-12-09 15:46:58 +0700

    saat menekan tombol kembali halaman 404, dismiss semua toast

--- pages/404.js
+++ pages/404.js
@@ -27,8 +27,15 @@ export default function Custom404 (props) {
   }, [])
 
   useEffect(() => {
-    if (countDown === 0) router.push('/')
+    if (countDown === 0) {
+      router.push('/')
+    }
   }, [countDown])
+
+  const handleClick = () => {
+    toast.dismiss()
+    router.push('/')
+  }
 
   return (
     <>
@@ -39,11 +46,9 @@ export default function Custom404 (props) {
             <p className='text-2xl md:text-3xl font-light leading-normal mb-3'>
               Maaf, halaman yang Anda kunjungi tidak ditemukan
             </p>
-            <Link href='/'>
-              <a className='px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-400 active:bg-blue-600 hover:bg-blue-500'>
-                Kembali ke halaman utama
-              </a>
-            </Link>
+            <button onClick={handleClick} className='px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-400 active:bg-blue-600 hover:bg-blue-500'>
+              Kembali ke halaman utama
+            </button>
           </div>
         </div>
       </div>
