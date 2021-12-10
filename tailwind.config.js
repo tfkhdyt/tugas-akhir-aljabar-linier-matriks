fed501c5d45bd6145b697df03b07bcdf750ff9dc
tfkhdyt <ta142fa@gmail.com>
2021-12-10 07:44:31 +0700

    migrasi ke tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,13 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["active", "group-focus"],
     },
   },
   plugins: [],
