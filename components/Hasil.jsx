6b1d8871562870094ad929a0e729b01ca58a1064
tfkhdyt <ta142fa@gmail.com>
2021-12-10 21:23:10 +0700

    ngecilin ukuran teks hasil dan nambah overflow-x pada components hasil

--- components/Hasil.jsx
+++ components/Hasil.jsx
@@ -8,8 +8,8 @@ export default function Hasil () {
   return (
     <div className='w-auto bg-blue-50 hover:bg-blue-100 rounded-lg shadow-md hover:shadow-lg p-6 flex flex-col transition duration-500'>
       <p className='text-xl font-semibold text-gray-800'>Hasil</p>
-      <div>
-        <p className='text-sm md:text-base text-gray-600'>
+      <div className='overflow-x-auto'>
+        <p className='text-sm lg:text-base text-gray-600'>
           <Provider
             options={{
               showMathMenu: false,
--- components/HasilAritmatika.jsx
+++ components/HasilAritmatika.jsx
@@ -8,8 +8,8 @@ export default function Hasil () {
   return (
     <div className='w-auto h-auto bg-blue-50 hover:bg-blue-100 rounded-lg shadow-md hover:shadow-lg p-6 flex flex-col transition duration-500'>
       <p className='text-xl font-semibold text-gray-800'>Hasil</p>
-      <div className='md:mt-14'>
-        <div className='text-gray-600 text-sm md:text-base lg:text-lg'>
+      <div className='md:mt-14 overflow-x-auto'>
+        <div className='text-gray-600 text-xs lg:text-base'>
           <Provider
             options={{
               showMathMenu: false,
