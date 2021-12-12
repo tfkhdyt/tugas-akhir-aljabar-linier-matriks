5c38c65952a3ba622237a58892bf16668abe88a0
tfkhdyt <ta142fa@gmail.com>
2021-12-12 20:17:57 +0700

    pindahin footer mobile ke bawah

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import Data from '../config'
 export default function Footer ({ config, versi }) {
   return (
     <footer
-      className={`bottom-0 inset-x-0 p-4 mb-2 font-light flex flex-col justify-center items-center text-center text-gray-500 ${config}`}
+      className={`bottom-0 inset-x-0 p-4 lg:mb-2 font-light flex flex-col justify-center items-center text-center text-gray-500 ${config}`}
     >
       <p>Built with ♥️ by Kelompok 2</p>
       <Link
