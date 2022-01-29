491435ee0f77712cea9307b9c4220c45fb4ca87c
tfkhdyt <ta142fa@gmail.com>
2022-01-30 06:35:36 +0700

    ganti font ke Fira Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
-
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}", 
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   // darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
-      },
+    fontFamily: {
+      body: ['Fira Sans', 'sans-serif'],
     },
   },
   plugins: [],
